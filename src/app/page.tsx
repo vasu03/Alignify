c8f97a054fc304decb9c8b1097ed54814bebf17b
vasu03 <vasurmakadia@gmail.com>
2024-12-19 18:05:56 +0530

    [setup] : Initial Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+// Importing required modules
+import React from "react"
+
+// Defining the page component
+const page = () => {
+
+	// TSX for rendering the page component
+	return (
+		<div>page</div>
+	);
+};
+
+// Exporting the page component
+export default page;
